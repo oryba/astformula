c688c8541d8f14f51e404c680672fed3802a058b
Oleh Rybalchenko <34271984+oryba@users.noreply.github.com>
2021-07-23 16:26:58 +0300

    Cover slices with tests

--- tests/test_default.py
+++ tests/test_default.py
@@ -16,6 +16,10 @@ def test_operators(engine):
         {"statement": "-1 / -5", "variables": {}, "result": Decimal('0.2')},
         {"statement": "1 / -1", "variables": {}, "result": -1},
         {"statement": "0 + -0", "variables": {}, "result": 0},
+        {"statement": "a[:(1 + 2)]", "variables": {"a": [1, 2, 3, 4, 5]}, "result": [1, 2, 3]},
+        {"statement": "a[1:3]", "variables": {"a": [1, 2, 3, 4, 5]}, "result": [2, 3]},
+        {"statement": "a[(1 + 2):]", "variables": {"a": [1, 2, 3, 4, 5]}, "result": [4, 5]},
+        {"statement": "a[::(1 + 1)]", "variables": {"a": [1, 2, 3, 4, 5]}, "result": [1, 3, 5]},
     ]
 
     executor(engine, base_const_tests)
