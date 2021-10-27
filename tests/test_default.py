4101144fdbdf51ce581bcf70f168c1c243453214
Oleh Rybalchenko <rv.oleg.ua@gmail.com>
2021-10-27 19:29:19 +0300

    Implement dict comprehensions

--- tests/test_default.py
+++ tests/test_default.py
@@ -20,6 +20,18 @@ def test_operators(engine):
         {"statement": "a[1:3]", "variables": {"a": [1, 2, 3, 4, 5]}, "result": [2, 3]},
         {"statement": "a[(1 + 2):]", "variables": {"a": [1, 2, 3, 4, 5]}, "result": [4, 5]},
         {"statement": "a[::(1 + 1)]", "variables": {"a": [1, 2, 3, 4, 5]}, "result": [1, 3, 5]},
+        {"statement": "[b ** 2 for b in a]", "variables": {"a": [1, 2, 3, 4, 5]}, "result": [1, 4, 9, 16, 25]},
+        {"statement": "[i for i, b in enumerate(a) if b >= 4]", "variables": {"a": [1, 2, 3, 4, 5]}, "result": [3, 4]},
+        {
+            "statement": "{i + 1: b ** 2 for i, b in enumerate(a) if i < 3}",
+            "variables": {"a": [1, 2, 3, 4, 5]},
+            "result": {1: 1, 2: 4, 3: 9}
+        },
+        {
+            "statement": "{i + 1: b ** 2 for i, b in enumerate(a) if i < 3 and b ** 0.5 > 1.5}",
+            "variables": {"a": [1, 2, 3, 4, 5]},
+            "result": {3: 9}
+        },
     ]
 
     executor(engine, base_const_tests)
